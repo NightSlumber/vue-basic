e6b27d884fde1f08cee35e63915daca73c50654a
night <night.slumber@outlook.com>
2024-05-05 16:35:37 +0800

    第一次尝试把代码上传到github

---
+++ 19-单文件组件/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <School/>
+    <Student/>
+  </div>
+</template>
+
+<script>
+    // 导入组件
+    import School from './School'
+    import Student from './Student'
+
+    export default {
+        name: 'App',
+        components: {
+            School,
+            Student
+        }
+    }
+</script>
